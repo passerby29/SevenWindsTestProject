3efab3da4c8f01b10228a585201881acf01d9624
Yusuf Muratov <78204469+passerby29@users.noreply.github.com>
2023-12-22 14:23:17 +0300

    Revert "Добавлены файлы в пакет Domain."

--- app/src/main/java/dev/passerby/seven_winds_test/domain/models/MenuItemModel.kt
+++
@@ -1,8 +0,0 @@
-package dev.passerby.seven_winds_test.domain.models
-
-data class MenuItemModel(
-    val id: Int,
-    val imageURL: String,
-    val name: String,
-    val price: Int
-)
--- app/src/main/java/dev/passerby/seven_winds_test/domain/usecases/LoadMenuListUseCase.kt
+++
@@ -1,7 +0,0 @@
-package dev.passerby.seven_winds_test.domain.usecases
-
-import dev.passerby.seven_winds_test.domain.repos.MenuRepository
-
-class LoadMenuListUseCase(private val repository: MenuRepository) {
-    suspend operator fun invoke(id: Int) = repository.loadMenuList(id)
-}
